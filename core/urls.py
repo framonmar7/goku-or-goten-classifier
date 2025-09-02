87c78f53b45952065ddb822e78e92048f89b1680
Francisco Jesús <framonmar7@gmail.com>
2025-09-02 11:37:51 +0200

    chore: remove unused code and files

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
 from django.urls import path, include
 from frontend.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path("api/", include("backend.urls")),
     path('', index),
 ]
